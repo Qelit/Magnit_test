967ca7e6a7485d63f121711628656527236c9e03
Qelit <Qelit@yandex.ru>
2017-10-09 23:05:34 +0300

    1st draw line

--- app/src/main/java/qelit/magnit_test/MainActivity.java
+++ app/src/main/java/qelit/magnit_test/MainActivity.java
@@ -2,12 +2,17 @@ package qelit.magnit_test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    ListView lvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvMain = (ListView) findViewById(R.id.lvMain);
+
     }
 }
