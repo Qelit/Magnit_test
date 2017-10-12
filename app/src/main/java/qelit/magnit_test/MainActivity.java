1ba3766ed22152292d8bfd3977938866acc5bec4
Qelit <Qelit@yandex.ru>
2017-10-12 22:51:25 +0300

    Add toolbar

--- app/src/main/java/qelit/magnit_test/MainActivity.java
+++ app/src/main/java/qelit/magnit_test/MainActivity.java
@@ -2,9 +2,16 @@ package qelit.magnit_test;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     ListView lvMain;
     Integer[] numbers = new Integer[100];
 
@@ -13,6 +20,12 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setDisplayShowTitleEnabled(true);
+
         for (int i = 0; i < 100; i++){
             numbers[i] = i;
         }
@@ -20,5 +33,17 @@ public class MainActivity extends Activity {
         lvMain = (ListView) findViewById(R.id.lvMain);
         MyAdapter adapter = new MyAdapter(this, numbers);
         lvMain.setAdapter(adapter);
+        lvMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 }
